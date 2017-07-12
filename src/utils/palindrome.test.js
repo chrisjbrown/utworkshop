1a791d88151459af6474012a842052abb9ab9fe7
Chris Brown <chrisjosbrown@gmail.com>
2017-07-12 12:48:48 +0200

    palindrome test

---
+++ src/utils/palindrome.test.js
@@ -0,0 +1,11 @@
+// import { isPalindrome } from './palindrome.js';
+
+// describe('isPalindrome', () => {
+//   it('should return true if is palindrome', () => {
+
+//   });
+
+//   it('should return false if not palindrome', () => {
+    
+//   });
+// });
